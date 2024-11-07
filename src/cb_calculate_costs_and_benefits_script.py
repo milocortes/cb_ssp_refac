91a530b1c3cea4ebc4f4cb1b5712ae6cd28da26a
Hermilo Cortes <hermilocg@hotmail.com>
2024-11-07 09:25:59 -0600

    actualiza main

--- src/cb_calculate_costs_and_benefits_script.py
+++ src/cb_calculate_costs_and_benefits_script.py
@@ -6,7 +6,7 @@
 
 from general_ssp_utilities import *
 from cb_config import *
-from cb_utilities import cb_calculate_system_costs, cb_calculate_transformation_costs
+from cb_utilities import cb_calculate_system_costs, cb_calculate_transformation_costs,cb_process_interactions
 from data_reader import CBFilesReader
 from cb_config import * 
 import pickle
@@ -24,12 +24,15 @@ def build_path(PATH : List[str]) -> str:
 CB_REFAC_PATH = build_path([os.getcwd(), ".."])
 CB_REFAC_DATA_PATH = build_path([CB_REFAC_PATH, "data"])
 STRATEGY_SPECIFIC_CB_PATH_FILES = build_path([CB_REFAC_DATA_PATH, "strategy_specific_cb_files"])
-SSP_RESULTS_DATA_PATH = build_path([CB_REFAC_DATA_PATH, "ssp_model_results"])
+#SSP_RESULTS_DATA_PATH = build_path([CB_REFAC_DATA_PATH, "ssp_model_results"])
+SSP_RESULTS_DATA_PATH = build_path([CB_REFAC_DATA_PATH, "ssp_model_results", "iran"])
 DEFINITION_FILES_PATH = build_path([CB_REFAC_DATA_PATH, "definition_files"])
 
 OUTPUT_PATH = build_path([CB_REFAC_PATH, "output"])
 
-data_filename = build_path([SSP_RESULTS_DATA_PATH, "sisepuede_results_sisepuede_run_ssp.csv"] )
+#data_filename = build_path([SSP_RESULTS_DATA_PATH, "sisepuede_results_sisepuede_run_ssp.csv"] )
+#data_filename = build_path([SSP_RESULTS_DATA_PATH, "sisepuede_results_sisepuede_run_iran.csv"] )
+data_filename = build_path([SSP_RESULTS_DATA_PATH, "iran.csv"] )
 primary_filename = build_path([SSP_RESULTS_DATA_PATH, "ATTRIBUTE_PRIMARY.csv"])
 strategy_filename = build_path([SSP_RESULTS_DATA_PATH, "ATTRIBUTE_STRATEGY.csv"])
 
@@ -44,6 +47,9 @@ data = output_file.copy()
 run_attributes = ssp_merge_run_attributes(primary_filename, strategy_filename)
 merged_data = run_attributes[['primary_id', 'strategy_code', 'future_id']].merge(right = data, on='primary_id')
 data = merged_data.copy()
+
+### Convertimos BASE ---> LNDU:PLUR
+data.loc[data.strategy_code=='BASE', "strategy_code"]="LNDU:PLUR"
 
 #clean the data of furnace gas and crude
 temp_data_cols = data.columns
@@ -79,14 +85,45 @@ SSP_GLOBAL_list_of_variables = list(set(data.columns) - set(SSP_GLOBAL_SIMULATIO
 #-------------REMOVE BASE FOR COST BENEFIT ANALYSIS------------
 data = data.query("strategy_code!='BASE'").reset_index(drop=True)
 
+#data[""]{i:j for i,j in pd.read_csv(primary_filename)[["primary_id", "strategy_id"]].to_records(index = False)}
 #-------------READ DEFINITION FILES IN AND CALCULATE COSTS AND BENEFITS----------
 
 #maps strategies to transformations, from James
 #This file tells us which transformation in is in each strategy
 strategy2tx = pd.read_csv(build_path([DEFINITION_FILES_PATH, 'attribute_strategy_code.csv']))
 
+## Agregamos las estrategias 6003 : 'PFLO:CONSTRAINED', 6004 : 'PFLO:TECHNOLOGICAL_ADOPTION',6005 :'PFLO:UNCONSTRAINED'
+ssp_strategy_definition_james = pd.read_csv("/home/milo/Documents/egtp/SISEPUEDE/ejecuciones_ssp/WB/sisepuede_region_nbs/iran/transformations/strategy_definitions.csv")
+
+nuevas_strategy2tx = []
+
+mapp_id2codename = {
+     6003 : 'PFLO:CONSTRAINED', 
+     6004 : 'PFLO:TECHNOLOGICAL_ADOPTION',
+     6005 :'PFLO:UNCONSTRAINED'
+}
+
+for id_tx in [6003, 6004, 6005]:
+    nuevas_strategy2tx.append(
+        [mapp_id2codename[id_tx]] + [int(i in ssp_strategy_definition_james.set_index("strategy_id").loc[id_tx].transformation_specification.split("|")) for i in strategy2tx.columns[1:] ]
+    )
+
+nuevas_strategy2tx = pd.DataFrame(nuevas_strategy2tx, columns=strategy2tx.columns)
+
+
+strategy2tx = pd.concat([strategy2tx, nuevas_strategy2tx], ignore_index = True)
+
 #tells us which strategies to evaluate costs and benefit iffor
 strategy_cost_instructions = pd.read_csv(build_path([DEFINITION_FILES_PATH, 'strategy_cost_instructions.csv']))
+
+new_strategy_cost_instructions = pd.DataFrame(
+    [
+       [mapp_id2codename[id_tx]]*2 + ["LNDU:PLUR",1,1]  for id_tx in [6003, 6004, 6005]
+    ],
+    columns = ['strategy', 'strategy_code', 'comparison_code', 'evaluate_system_costs','evaluate_transformation_costs']
+)
+
+strategy_cost_instructions = pd.concat([strategy_cost_instructions, new_strategy_cost_instructions], ignore_index=True)
 
 #the list of all the cost factor files in the system, and the functions they should be evaluated with
 cost_factor_names = pd.read_csv(build_path([DEFINITION_FILES_PATH, 'system_cost_factors_list.csv']))  
@@ -110,8 +147,59 @@ results_tx = cb_calculate_transformation_costs(data,
 RESULTS_SYSTEMS_PATH = build_path([OUTPUT_PATH, "python", "results_system_python.csv"])
 RESULTS_TX_SYSTEMS_PATH = build_path([OUTPUT_PATH, "python", "results_tx_python.csv"])
 
-results_system.to_csv(RESULTS_SYSTEMS_PATH, index = False)
-results_tx.to_csv(RESULTS_TX_SYSTEMS_PATH, index = False)
+
+#combine the results
+results_all = pd.concat([results_system, results_tx], ignore_index = True)
+
+SSP_GLOBAL_list_of_cbvars = results_all["variable"].unique()
+
+
+#-------------POST PROCESS SIMULATION RESULTS---------------
+#Post process interactions among strategies that affect the same variables
+postprocess_interactions = pd.read_csv(build_path([DEFINITION_FILES_PATH, 'strategy_interaction_definitions.csv']))
+
+results_all_pp = cb_process_interactions(results_all, strategy2tx, postprocess_interactions)
+
+#POST PROCESS TRANSPORT CB ISSUES (These should be addressed properly in the next round of analysis)
+#congestion and safety benefits should be 0 in strategies that only make fuel efficiency gains and fuel switching
+
+cond_transport_cb_issues_better_base = (results_all_pp["strategy_code"]=="PFLO:BETTER_BASE") & (results_all_pp["variable"].apply(lambda x : any(k in x for k in ["congestion", "road_safety"])))
+cond_transport_cb_issues_supply_side_tech = (results_all_pp["strategy_code"]=="PFLO:SUPPLY_SIDE_TECH") & (results_all_pp["variable"].apply(lambda x : any(k in x for k in ["congestion", "road_safety"])))
+
+results_all_pp.loc[cond_transport_cb_issues_better_base, "value"] = 0.0
+results_all_pp.loc[cond_transport_cb_issues_supply_side_tech, "value"] = 0.0
+
+#in ALL, cut the benefits in half to be on the safe side.
+cond_cut_ben_in_half_all_plur =  (results_all_pp["strategy_code"]=="PFLO:ALL_PLUR") & (results_all_pp["variable"].apply(lambda x : any(k in x for k in ["congestion", "road_safety"])))
+cond_cut_ben_in_half_all_non_stopping_def_plur =  (results_all_pp["strategy_code"]=='PFLO:ALL_NO_STOPPING_DEFORESTATION_PLUR') & (results_all_pp["variable"].apply(lambda x : any(k in x for k in ["congestion", "road_safety"])))
+
+results_all_pp.loc[cond_cut_ben_in_half_all_plur, "value"] *= 0.5 
+results_all_pp.loc[cond_cut_ben_in_half_all_non_stopping_def_plur, "value"] *= 0.5 
+
+#POST PROCESS WASO CB ISSUES
+#where moving from incineration to landfilling appears to have benefits
+#when this move should probably not occur
+cond_waso_cb_issue =  (results_all_pp["strategy_code"]=="PFLO:SUPPLY_SIDE_TECH") & (results_all_pp["variable"].apply(lambda x : any(k in x for k in ["cb:waso:technical_cost:waste_management"])))
+results_all_pp.loc[cond_waso_cb_issue, "value"] = 0.0
+
+
+#SHIFT any stray costs incurred from 2015 to 2025 to 2025 and 2035
+results_all_pp_before_shift = results_all_pp.copy() #keep copy of earlier results just in case/for comparison
+res_pre2025 = results_all_pp.query(f"time_period<{SSP_GLOBAL_TIME_PERIOD_TX_START}")#get the subset of early costs
+res_pre2025["variable"] = res_pre2025["variable"] + "_shifted" + (res_pre2025["time_period"]+SSP_GLOBAL_TIME_PERIOD_0).astype(str)#create a new variable so they can be recognized as shifted costs
+res_pre2025["time_period"] = res_pre2025["time_period"]+SSP_GLOBAL_TIME_PERIOD_TX_START #shift the time period
+
+results_all_pp = pd.concat([results_all_pp, res_pre2025], ignore_index = True) #paste the results
+
+results_all_pp.loc[results_all_pp["time_period"]<SSP_GLOBAL_TIME_PERIOD_TX_START,'value'] = 0 #set pre-2025 costs to 0
+
+
+#Write
+CB_OUTPUT_FILENAME = build_path([OUTPUT_PATH, "cost_benefit_results.csv"])
+results_all_pp.to_csv(CB_OUTPUT_FILENAME, index = False)
+
+#results_system.to_csv(RESULTS_SYSTEMS_PATH, index = False)
+#results_tx.to_csv(RESULTS_TX_SYSTEMS_PATH, index = False)
 
 
 """
@@ -132,3 +220,6 @@ logger = logging.getLogger("task_app")
 DATA_FILE_PATH = "/home/milo/Documents/egtp/SISEPUEDE/COST_BENEFITS/refactorizacion/local/data"
 cb_reader = CBFilesReader(DATA_FILE_PATH, logger)
 """
+
+
+
---
+++ src/reshaping_cbData.r
@@ -0,0 +1,44 @@
+
+#read all folders 
+ dir.data <- "/home/milo/Documents/egtp/SISEPUEDE/CB/cb_ssp_refac/output/"
+ target_cb_file <- "cost_benefit_results.csv"
+ cb_data <-read.csv(paste0(dir.data,target_cb_file))
+ cb_chars <- data.frame(do.call(rbind, strsplit(as.character(cb_data$variable), ":")))
+ colnames(cb_chars) <- c("name","sector","cb_type","item_1","item_2")
+ cb_data <- cbind(cb_data,cb_chars)
+ cb_data$value <- cb_data$value/1e6
+
+#remove shifted 
+ dim(cb_data)
+ cb_data <- subset(cb_data,grepl("shifted",cb_data$item_2)==FALSE)
+ dim(cb_data)
+ ids <- unique(cb_data$variable)
+ ids <- subset(ids,grepl("shifted2",ids)==FALSE)
+#clean  
+ cb_data <- subset(cb_data,grepl("shifted2",cb_data$variable)==FALSE)
+ dim(cb_data)
+
+#add Year 
+cb_data$Year <- cb_data$time_period+2015
+
+#change strategy names 
+cb_data$strategy <- gsub("PFLO:UNCONSTRAINED", "Unconstrained Climate Action", cb_data$strategy )
+cb_data$strategy <- gsub("PFLO:CONSTRAINED", "Constrained Climate Action-Efficiency Only", cb_data$strategy )
+cb_data$strategy <- gsub("PFLO:TECHNOLOGICAL_ADOPTION", "Unconstrained Climate Action -Technology Adoption Scenario-", cb_data$strategy )
+
+#create strategy id 
+cb_data$strategy_id <- ifelse(cb_data$strategy=="Unconstrained Climate Action",6005,ifelse(cb_data$strategy=="Unconstrained Climate Action -Technology Adoption Scenario-",6004,6003))
+cb_data$ids <- paste(cb_data$variable,cb_data$strategy_id,sep=":")
+
+
+#edition to the table  
+#differentiate between opex and capex 
+cb_data$cb_type <- ifelse(cb_data$cb_type=="technical_cost" & cb_data$item_2=="opex","opex",cb_data$cb_type)
+cb_data$cb_type <- ifelse(cb_data$cb_type=="technical_cost", "capex",cb_data$cb_type)
+
+
+dir.out  <- "/home/milo/Documents/egtp/SISEPUEDE/CB/cb_ssp_refac/output/"
+write.csv(cb_data,paste0(dir.out,"cb_data.csv"),row.names=FALSE)
+
+
+
